1e86b7c686f338c59f6d586fe50f07a86e71caee
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-03-31 12:36:14 +0000

    logout bg

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Logout() {
 	return (
-		<div className="bg-yellow-200 h-screen">
+		<div className="bg-black h-screen">
       <div className="text-center">
         <h1 className="text-3xl font-bold text-red-700">Thoda Sabar Karlo Bhai.....</h1>
       </div>
