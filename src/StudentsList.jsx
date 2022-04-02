0745f521dee4b833a35e62f9cc87f168956d562a
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-02 04:21:21 +0000

    Students List PAge

--- src/StudentsList.jsx
+++ src/StudentsList.jsx
@@ -7,7 +7,7 @@ function StudentsList() {
 	return (
 		<div>
 
-<marquee className="text-red-500 font-bold text-3xl" > CodeYogi 2.0 Students </marquee>
+<marquee className="text-red-500 font-bold text-3xl" > CodeYogi 2.0 Students List </marquee>
       
          <TeamCard name="Vishal" title="CEO" photo="https://cdn.discordapp.com/attachments/944189291427430421/946420617199124500/vkjpg.jpg" linkdin="https://www.linkedin.com//" twitter="https://twitter.com/login" ></TeamCard>
       
