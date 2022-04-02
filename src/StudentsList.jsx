d4e03053de5d427b4d04332a240d39cc71a54492
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-02 04:20:54 +0000

    Marquee add

--- src/StudentsList.jsx
+++ src/StudentsList.jsx
@@ -6,6 +6,9 @@ import TeamCard from './TeamCard';
 function StudentsList() {
 	return (
 		<div>
+
+<marquee className="text-red-500 font-bold text-3xl" > CodeYogi 2.0 Students </marquee>
+      
          <TeamCard name="Vishal" title="CEO" photo="https://cdn.discordapp.com/attachments/944189291427430421/946420617199124500/vkjpg.jpg" linkdin="https://www.linkedin.com//" twitter="https://twitter.com/login" ></TeamCard>
       
 		</div>
