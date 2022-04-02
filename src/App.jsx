8823818113e2ffde1365d3a5e8ed6d983db98e57
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-02 03:55:42 +0000

    Students page add

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {Navigate, Routes, Route } from 'react-router-dom';
 import ProfilePage from './ProfilePage';
 import MainLayout from './MainLayout';
 import NotFound from './NotFound';
-
+import StudentsList from './StudentsList';
 
 function App() {
 	return (
@@ -16,6 +16,9 @@ function App() {
 			<Route path="/" element={<MainLayout />}>
 				<Route path="lectures" element={<LectureListPage />} />
 				<Route path="assigments" element={<AssigmentListPage />} />
+        		<Route path="Students" element={<StudentsList/>} />
+
+        
 				<Route path="profile" element={<ProfilePage />} />
 			</Route>
 			
--- src/SideBarPage.jsx
+++ src/SideBarPage.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function SideBarPage() {
 	return (
 		<div className="pr-2 bg-gray-800 w-64 flex flex-col h-screen justify-between ">
-			<div className="flex flex-col ml-4 ">
+			<div className="flex flex-col ml-4 mt-5 ">
 				<h1 className="text-white font-extrabold text-4xl pt-6 mb-5">
 					CodeYogi
 				</h1>
@@ -19,6 +19,11 @@ function SideBarPage() {
 				<Link to="/assigments" className="text-white font-medium text-md">
 					Assigments
 				</Link>
+
+	<Link to="/Students" className="text-white font-medium text-md">
+					Students
+				</Link>
+        
 			</div>
 			<div className="flex flex-col ml-4 mb-5">
 				<Link to="/profile" className="text-white font-medium text-md">
