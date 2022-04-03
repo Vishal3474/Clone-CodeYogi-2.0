04befb71c61ab6215a94abbe0275b88b66c73096
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-03 17:03:06 +0000

    padding

--- src/SideBarPage.jsx
+++ src/SideBarPage.jsx
@@ -8,33 +8,33 @@ import { GiGraduateCap,GiAbstract050 } from "react-icons/gi";
 function SideBarPage() {
 	return (
 		<div className="pr-2 bg-gray-800 w-64 flex flex-col h-screen justify-between ">
-			<div className="flex flex-col ml-4 mt-5 ">
+			<div className="flex flex-col ml-4 mt-4 ">
 				<h1 className="text-white font-extrabold text-4xl pt-6 mb-5">
 					CodeYogi
 				</h1>
 
-				<Link  to="/quiz" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2"><GiAbstract050/>
+				<Link  to="/quiz" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-1"><GiAbstract050/>
 					Quiz
 				</Link> 
-				<Link to="/lectures" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2"><IoMdTv/>
+				<Link to="/lectures" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-1"><IoMdTv/>
 					Lectures
 				</Link>
-				<Link to="/assigments" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2"> <IoMdWallet/>
+				<Link to="/assigments" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-1"> <IoMdWallet/>
 					Assigments
 				</Link>
 
-	<Link to="/Students" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2"><GiGraduateCap/>
+	<Link to="/Students" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-1"><GiGraduateCap/>
 					Students
 				</Link>
         
 			</div>
 			<div className="flex flex-col ml-4 mb-5">
-				<Link to="/profile" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2"> <IoIosContact />
+				<Link to="/profile" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-1"> <IoIosContact />
 					Profile
 				</Link  >
 				<a
 						href="https://app.codeyogi.io/login/"
-						className="text-white font-bold inline-block flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 text-xl"
+						className="text-white font-bold inline-block flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 text-xl mb-1"
 	
 					> < FiLogOut /> 
 						Logout
