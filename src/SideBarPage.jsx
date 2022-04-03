ab222d4bd88ed5d8feeb09d6a6c0e198ae4bfd7c
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-03 17:54:50 +0000

    c

--- src/SideBarPage.jsx
+++ src/SideBarPage.jsx
@@ -13,28 +13,28 @@ function SideBarPage() {
 					CodeYogi
 				</h1>
 
-				<Link  to="/quiz" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"><GiAbstract050/><span className="mr-2"></span>
+				<Link  to="/quiz" className="text-white font-medium text-lg flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"><GiAbstract050/><span className="mr-2"></span>
 					Quiz
 				</Link> 
-				<Link to="/lectures" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"><IoMdTv/><span className="mr-2"></span>
+				<Link to="/lectures" className="text-white font-medium text-lg flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"><IoMdTv/><span className="mr-2"></span>
 					Lectures
 				</Link>
-				<Link to="/assigments" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"> <IoMdWallet/><span className="mr-2"></span>
+				<Link to="/assigments" className="text-white font-medium text-lg flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"> <IoMdWallet/><span className="mr-2"></span>
 					Assigments
 				</Link>
 
-	<Link to="/Students" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"><GiGraduateCap/><span className="mr-2"></span>
+	<Link to="/Students" className="text-white font-medium text-lg flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"><GiGraduateCap/><span className="mr-2"></span>
 					Students
 				</Link>
         
 			</div>
 			<div className="flex flex-col ml-4 mb-5">
-				<Link to="/profile" className="text-white font-medium text-xl flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"> <IoIosContact /><span className="mr-2"></span>
+				<Link to="/profile" className="text-white font-medium text-lg flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 mb-3"> <IoIosContact /><span className="mr-2"></span>
 					Profile
 				</Link  >
 				<a
 						href="https://app.codeyogi.io/login/"
-						className="text-white font-bold inline-block flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 text-xl mb-1"
+						className="text-white font-bold inline-block flex items-center hover:bg-gray-700 hover:rounded-md hover:px-2 text-lg mb-1"
 	
 					> < FiLogOut /> <span className="mr-2"></span>
 						Logout
